cf6e58227ac15aa6d2f9ad214514f1259d95592a
biechi <haob.19@intl.zju.edu.cn>
2022-05-20 23:07:02 -0500

    update

--- queue/trigger.go
+++ queue/trigger.go
@@ -83,7 +83,7 @@ func ExecuteTask(task *Task) error {
 		logrus.Info("Cloned ", task.Payload.Pusher.Name+"/"+task.Payload.HeadCommit.ID)
 	}
 
-	/* dispatch other tasks to external programs */
+	/* dispatch other tasks to external bash program */
 	cmdBash := exec.Command("bash", "test.sh")
 	outputBash, errBash := cmdBash.Output()
 	if errBash != nil {
