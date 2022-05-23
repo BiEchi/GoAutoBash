2687153c26bb5317d9ab024ee7162a5b600596d6
biechi <haob.19@intl.zju.edu.cn>
2022-05-23 12:12:06 -0500

    update

--- queue/trigger.go
+++ queue/trigger.go
@@ -127,8 +127,6 @@ func ExecuteTask(task *Task) error {
 		execCommand(dir, "cp", "report/student.asm", "report/regression/student.asm")
 		execCommand(dir, "cp", "report/gold.asm", "report/regression/gold.asm")
 		execCommand(dir, "cp", "report/replay.sh", "report/regression/replay.sh")
-		execCommand(dir, "cp", "report/mem_alloc.asm", "report/regression/mem_alloc.asm")
-		execCommand(dir, "cp", "report/test_data.asm", "report/regression/test_data.asm")
 		/* we have previous run history, add the commits to list regTestList */
 		var regTestString string
 		fDirs, _ := ioutil.ReadDir("report" + "/" + task.Payload.Pusher.Name)
@@ -154,7 +152,7 @@ func ExecuteTask(task *Task) error {
 			execCommand(".", "docker", "run", "-P", "-v=/root/GoAutoBash/"+dir+"/report/regression:/home/klee/report/regression:Z", "liuzikai/klc3",
 				"klc3", "--test=report/regression/student.asm", "--gold=report/regression/gold.asm", "--use-forked-solver=false",
 				"--copy-additional-file=report/regression/replay.sh", "--max-lc3-step-count=200000", "--max-lc3-out-length=1100",
-				/*regTest*/ "report/regression/mem_alloc.asm", "report/regression/test_data.asm")
+				regTest)
 		}
 
 		execCommand(dir, "rm", "report/regression/gold.asm")
